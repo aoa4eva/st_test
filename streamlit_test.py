ff7b00bfbf2a1751322aaac73110b4d7a543eb64
aoa4eva <afua.ankomah@gmail.com>
2023-06-03 15:33:44 +0000

    Updated code

---
+++ streamlit_test.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import click
+import webbrowser
+from streamlit.components.v1 import html 
+
+
+st.set_page_config(page_title="Streamlit Test",page_icon=":fire:")
+
+# Option 1
+def launch_page():
+    click.launch("https://www.google.com")
+
+
+# Option 2
+clicked = st.button('Click me')
+if(clicked): 
+    webbrowser.open_new("http://www.google.com")
+
+# Option 3
+
+def open_page(item):
+    open_script=f"""<script type="text/javascript">window.open('{item}','_blank').focus();</script>"""
+    st.write(open_script)
+    html(open_script)
+
+st.button('Open link', on_click=open_page, args=('https://streamlit.io',))
